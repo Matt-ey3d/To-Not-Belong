fd2fbcaa20c09ef5297857ef35721552604b3d01
Mateus Silvestre <matthew.ytk@outlook.pt>
2026-03-24 11:45:45 +0000

    Roberto moves to the sides now
    Roberto will try to jump next time

--- Assets/Scripts/RobertoMover.cs
+++ Assets/Scripts/RobertoMover.cs
@@ -5,12 +5,13 @@ public class RobertoMover : MonoBehaviour
 {
     public Rigidbody2D Roberto;
 
-    private float forca = 0.1f;
+    private float forca = 5f;
+    private bool voar = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,6 +24,15 @@ public class RobertoMover : MonoBehaviour
         if (Keyboard.current.dKey.isPressed)
         {
             Roberto.AddForce(Vector2.right * forca);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("SALTA ROBERTO O CHÃO É LAVA");
+        if (collision.collider.CompareTag("Floor") && Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("PULINHO");
+            Roberto.AddForce(Vector2.up * forca);
         }
     }
 }
